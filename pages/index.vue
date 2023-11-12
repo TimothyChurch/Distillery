f2e438cc9953da5717738744a44eabe0ef4c9c22
timothy <timothychurch@me.com>
2023-11-12 16:32:13 -0600

    1

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,14 @@
 <script setup></script>
 
 <template>
-    <div>
-        <UPageHero class="flex items-center m-3 text-center px-12"
-            title="Come Join Us!!"
-            description="Come join us for our grand opening! We are so excited to finally be opening the dream business! Meet the distiller, see the equipment, have some snacks, and most importantly enjoy some drinks! Kids are Welcome!"
-            align="right"
-        >
-            <NuxtImg src="/images/landing-hero.png" alt="Galveston Distilling Company" class="mx-auto"/>
-        </UPageHero>
+    <div class="flex flex-col items-center gap-6">
+    <div class="flex justify-center">
+        <NuxtImg src="/images/landing-hero.png" alt="Galveston Distilling Company" class="h-3/4"/>
+        <div class="flex  flex-col text-center  my-auto gap-5">
+        <h1 class="text-7xl">Come Join Us!!</h1>
+        <p class="text-xl">Come join us for our grand opening!<br />We are so excited to finally be opening the dream business!<br /> Meet the distiller, see the equipment, have some snacks, <br />and most importantly enjoy some drinks! Kids are Welcome!</p>
+        </div>
     </div>
+    <p class="text-center text-xl max-w-5xl">We have big plans for things coming up! While we are starting off with just vodka, 2 gins, and a rum, we already have several barrels filled with aging products and many new recipes to try out. Eventually we will be setting up hands-on classes and tours to allow you to get a feel for everything that goes into making your favorite spirits and have fun learning. From new recipes, custom barrels, to hands-on experiences! Please stay tuned as we grow and are able to offer you even more!</p>
+</div>
 </template>
